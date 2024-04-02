5422de783624a5c61b9d5b24ae5fd1fe5ee94f89
romuloromulo <neoromulo@gmail.com>
2024-04-01 23:33:41 -0300

    criando dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
       <section>
         <HeroParallax products={products}></HeroParallax>
       </section>
-      <section className="">
+      <section className="mb-12">
         <LampComponent />
         <div className="flex flex-wrap items-center justify-center flex-col md:flex-row gap-8 -mt-72">
           <CardContainer className="inter-var ">
